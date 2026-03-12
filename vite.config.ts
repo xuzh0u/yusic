90057fd861b772f3e48bc6990089816bc106d663
Xuzhou Zheng <zhengxuzhou18@mails.ucas.edu.cn>
2026-03-12 05:36:27 +0000

    fix: update base path to /yusic/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bgm-generator/',
+  base: '/yusic/',
   build: {
     outDir: 'dist',
     sourcemap: true
